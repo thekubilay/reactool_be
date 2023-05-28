2cb3c0faab02ac09741d08ea062be38a1590009e
kubilayturgut <kubilay.turgut@gmail.com>
2023-05-28 13:51:51 +0900

    update (2)

--- reactool_be/dev.py
+++ reactool_be/dev.py
@@ -20,7 +20,6 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# Configure media files storage
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 EMAIL_HOST = config('EMAIL_HOST', default='localhost')
