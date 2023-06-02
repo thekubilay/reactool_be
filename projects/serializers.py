48076d356c5da3c3e5126296dcedd37449a59d75
kubilayturgut <kubilay.turgut@gmail.com>
2023-06-02 23:07:06 +0900

    update

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from projects.models import Project, ProjectGroup
 from routes.serializers import RouteSerializer
+from galleries.serializers import GallerySerializer
 
 
 class ProjectSerializer(serializers.ModelSerializer):
 	routes = RouteSerializer(many=True, read_only=True)
+	galleries = GallerySerializer(many=True, read_only=True)
 
 	class Meta:
 		model = Project
