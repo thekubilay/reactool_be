7537ae94a43907504b37158c763145aaeb680960
kubilayturgut <kubilay.turgut@gmail.com>
2023-06-02 22:26:08 +0900

    update

--- galleries/urls.py
+++ galleries/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from galleries.views import GalleryListAPIView
 
 urlpatterns = [
-	path('galleries/<int:project_id>/', GalleryListAPIView.as_view(), name='gallery_list'),
+	path('<int:project_id>/galleries/', GalleryListAPIView.as_view(), name='gallery_list'),
 ]
