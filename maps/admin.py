d495a758ee0cc95581890940524492d8eaed86a1
kubilayturgut <kubilay.turgut@gmail.com>
2023-06-04 20:44:34 +0900

    update

--- maps/admin.py
+++ maps/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from maps.models import Map, MapCategory
+from maps.models import Map, MapCategory, MapImage
 
 
+@admin.register(Map)
 class MapAdmin(admin.ModelAdmin):
 	readonly_fields = ('id',)
 	list_display = ('id', 'name', 'address', 'category',)
@@ -10,11 +11,26 @@ class MapAdmin(admin.ModelAdmin):
 	search_fields = ('project__name', 'name', 'address', 'category')
 	list_per_page = 25
 
-
 	def formfield_for_foreignkey(self, db_field, request, **kwargs):
 		if db_field.name in ('project',):  # Check if it's either company or project_group
 			kwargs["queryset"] = db_field.related_model.objects.order_by('name')
 		return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
-admin.site.register(Map, MapAdmin)
+@admin.register(MapCategory)
+class MapCategoryAdmin(admin.ModelAdmin):
+	readonly_fields = ('id',)
+	list_display = ('id', 'name',)
+	list_display_links = ('id',)
+	search_fields = ('name',)
+	list_per_page = 25
+
+
+@admin.register(MapImage)
+class MapImageAdmin(admin.ModelAdmin):
+	readonly_fields = ('id',)
+	list_display = ('id', 'map', 'image',)
+	list_display_links = ('id',)
+	search_fields = ('map__name', 'image',)
+	list_per_page = 25
+
