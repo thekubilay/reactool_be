4cf1457de912e9daafe224b0f622a05359c939b7
kubilayturgut <kubilay.turgut@gmail.com>
2023-06-05 20:30:57 +0900

    update

--- plans/urls.py
+++ plans/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from plans.views import PlanListAPIView
 
 urlpatterns = [
-	path('<int:project_id>/maps/', PlanListAPIView.as_view(), name='map_list'),
+	path('<int:project_id>/plans/', PlanListAPIView.as_view(), name='plan_list'),
 ]
